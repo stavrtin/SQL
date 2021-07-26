3a9ed5dec332cb5093d5ade587ef8d932ea67daa
stavrtin <stavrtin@yandex.ru>
2021-07-26 15:08:17 +0300

    querty fo DB

--- HR_db_source-01.sql
+++ HR_db_source-01.sql
@@ -37,6 +37,7 @@ JOIN post p ON post_id = p.id
 */
 
 -- 4. Общая потребность фирмы в профессиях:
+/*
 SELECT 	post_name,
 		sum(post_vocant) AS 'Кол-во вакантных',
 		sum(post_occupied) AS 'Кол-во занятых',
@@ -45,13 +46,41 @@ FROM staff s
 JOIN department d ON s.department_id = d.id 
 JOIN post p ON post_id = p.id 
 GROUP BY post_id 
+*/
+-- 5. Подбор подходящих резюме по требованиям департаментов
 
--- 5. количество подходящих резюме
+SELECT  post AS 'На должность',
+		resume.age AS 'Требуемый возраст',
+		salary_max AS 'Мах оклад',
+		profession AS 'Профессия канд-та', 
+		rh.age AS 'Возраст канд-та',
+		salary AS 'Ожидание по зп',
+		lastname AS 'ФИО канд-та',
+		phone AS 'Телефон'
+		FROM resume JOIN request_hr rh 
+WHERE post = profession AND resume.age > rh.age AND salary <= salary_max
+GROUP BY post ;
 
 -- 5. бюджет ФОТ 1го департамента на 2022 год  
--- 6. рейтинг
--- 7. пожбор сотрудников, работавших ранее вместе
+-- количество сотрудников, выполнивших работу в период с ... по
+
+-- 6. Изменеие рейтинга выбранному работнику после по решению руководителя
+/*
+SET @worker_firstname =  ' Захар';
+SET @worker_lastname =  ' Сафронов';
+UPDATE profiles 
+SET rating = 90
+WHERE workers_id = (SELECT id
+					FROM workers 
+					WHERE workers.firstname = @worker_firstname AND workers.lastname =  @worker_lastname
+					);
+*/
+
+-- 7. подбор сотрудников, работавших ранее вместе
+		
 -- 8. Оформление соискателя на должность в департамент
+-- перенос сведений из резюме в трудовую книжку (личное дело)
+
 -- 9. Внесение изменений в личное дело сотудника (изменение состава семьи)
--- 10.Внесение изменений в трудовые книжки. 
+
 
